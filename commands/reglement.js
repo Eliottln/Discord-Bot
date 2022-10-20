45fbe3c581e5f278a8ec1dcd7a290c0dc221a6e2
Eliott LE NORMAND <eliott.le-normand@etu.univ-lyon1.fr>
2022-10-20 19:31:03 +0200

    reglement command with button interaction

---
+++ commands/reglement.js
@@ -0,0 +1,47 @@
+const {ActionRowBuilder, ButtonBuilder, EmbedBuilder, ButtonStyle, SlashCommandBuilder} = require("discord.js");
+
+const button = new ActionRowBuilder()
+    .addComponents(
+        new ButtonBuilder()
+            .setCustomId('accept-button')
+            .setLabel('Accepter')
+            .setStyle(ButtonStyle.Success),
+    );
+
+const embed = new EmbedBuilder()
+    .setColor('#24b345')
+    .setTitle('--- 𝐑𝐄𝐆𝐋𝐄𝐌𝐄𝐍𝐓 ---')
+    .setDescription('A survoler si vous le souhaitez mais à respecter obligatoirement !')
+    .addFields([
+        {
+            name: '》𝐕𝐔𝐋𝐆𝐀𝐑𝐈𝐓𝐄《',
+            value: 'Les insultes, le racisme, l\'antisémitisme, le sexisme, homophobie sont tolérés dans la limite du raisonnable. En cas d\'abus, des mesures peuvent être prises.',
+        },
+        {
+            name: '》𝐇𝐀𝐑𝐂𝐄𝐋𝐄𝐌𝐄𝐍𝐓《',
+            value: 'Toutes formes d\'incitation à la haine que cela soit : suicide, piratage, violence, haine ect.. sont 𝘀𝘁𝗿𝗶𝗰𝘁𝗲𝗺𝗲𝗻𝘁 interdites.',
+        },
+        {
+            name: '》𝐕𝐈𝐄 𝐏𝐑𝐈𝐕𝐄𝐄《',
+            value: 'Les disputes personnelles se règlent en privé.\n' +
+                'Le partage non consenti d\'informations personnelles d\'une autre personne pourront être punies.'
+        },
+        {
+            name: '》𝐄𝐍 𝐂𝐀𝐒 𝐃𝐄 𝐍𝐎𝐍 𝐑𝐄𝐒𝐏𝐄𝐂𝐓 𝐃𝐄𝐒 𝐑𝐄𝐆𝐋𝐄𝐒《',
+            value: '1 avertissement\n' +
+                '3 avertissements = 1 ban temporaire\n' +
+                '2 ban = ban définitif\n' +
+                '\n' +
+                'Ce serveur se base sur une relation de confiance. Vous êtes une personne respectueuse et vous savez faire preuve de savoir-vivre; sinon des sanctions auront lieu sans discussion.\n' +
+                '\n' +
+                'Cliquez sur le bouton pour accepter le règlement.'
+        }]);
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('reglement')
+        .setDescription('Affiche le règlement du serveur'),
+    async execute(interaction) {
+        await interaction.reply({embeds: [embed], components: [button]})
+    },
+}
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,20 +3,34 @@ const { Events } = require('discord.js');
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
-        if (!interaction.isChatInputCommand()) return;
+        if (interaction.isChatInputCommand()) {
 
-        const command = interaction.client.commands.get(interaction.commandName);
+            const command = interaction.client.commands.get(interaction.commandName);
 
-        if (!command) {
-            console.error(`No command matching ${interaction.commandName} was found.`);
-            return;
-        }
+            if (!command) {
+                console.error(`No command matching ${interaction.commandName} was found.`);
+                return;
+            }
 
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(`Error executing ${interaction.commandName}`);
-            console.error(error);
-        }
+            try {
+                await command.execute(interaction);
+            } catch (error) {
+                console.error(`Error executing ${interaction.commandName}`);
+                console.error(error);
+            }
+
+        } else if (interaction.isButton()) {
+
+            if (interaction.customId === "accept-button") {
+                try {
+                    await interaction.member.roles.add('962621385216249877');
+                    await interaction.reply({content: 'Tu es maintenant membre du serveur !', ephemeral: true});
+                } catch (error) {
+                    console.error(`Error executing ${interaction.customId}`);
+                    console.error(error)
+                }
+            }
+
+        } else console.error(`No command or button matching ${interaction} was found.`);
     },
 };
